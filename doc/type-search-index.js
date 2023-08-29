88d36d20187a614c3cb3f42ac0f74b0d93234471
EDUARDO <eiglesias@platkmframework.org>
2023-08-29 17:24:19 +0200

    init commit

---
+++ doc/type-search-index.js
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ *   Copyright(c) 2023 the original author or authors.
+ *  
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *  
+ *        https://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *******************************************************************************/
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.platkmframework.databasereader.model","l":"Column"},{"p":"org.platkmframework.databasereader.model","l":"ColumnMapping"},{"p":"org.platkmframework.databasereader.model","l":"DbConnection"},{"p":"org.platkmframework.databasereader.model","l":"DbMigrationMapping"},{"p":"org.platkmframework.databasereader.model","l":"DbMigrationProject"},{"p":"org.platkmframework.databasereader.model","l":"FkContraint"},{"p":"org.platkmframework.databasereader.model","l":"ImportedKey"},{"p":"org.platkmframework.databasereader.model","l":"IndexContraint"},{"p":"org.platkmframework.databasereader.model","l":"PkContraint"},{"p":"org.platkmframework.databasereader.model","l":"SourceDataBase"},{"p":"org.platkmframework.databasereader.model","l":"Table"},{"p":"org.platkmframework.databasereader.model","l":"TableMapping"},{"p":"org.platkmframework.databasereader.model","l":"TargetDataBase"}];updateSearchResults();
